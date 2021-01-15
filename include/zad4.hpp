e939b8b3249ecba06fe8ff5246e7d93ea4320731
SajMich <michal.sajewicz.stud@pw.edu.pl>
2021-01-15 08:18:27 +0000

    v7

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -4,3 +4,17 @@
 #include <vector>
 
 // tutaj funkcja stworzZespol
+std::vector<Artysta> stworzZespol(int n, const std::string& s)
+{
+	if (s.size() != n)
+	{
+		//std::logic_error wyjatek{0};
+		throw std::logic_error(std::string("Blad"));
+	}
+	std::vector<Artysta> tmp;
+	for (int i=0;i<n;i++)
+	{
+		tmp.push_back(Artysta(s, i));
+	}
+
+};
