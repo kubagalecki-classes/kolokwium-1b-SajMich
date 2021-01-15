cb0d237a2b36a0b5b8e63682158167d9beabfb2f
SajMich <michal.sajewicz.stud@pw.edu.pl>
2021-01-15 07:23:05 +0000

    v1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -7,3 +7,20 @@
 #include <string>
 
 // tutaj klasa Artysta
+
+class Artysta
+{
+public:
+	Artysta()
+	{
+		pseudonim = "Anonim";
+		sluchacze = 0;
+	}
+	Artysta(std::string s, unsigned u) : pseudonim(s), sluchacze(u) {};
+	void graj(std::ostream& os) const { os << pseudonim << ": " << sluchacze<<"\n"; }
+	const std::string& getPseudonim() const { return pseudonim; }
+	unsigned getSluchacze() const { return sluchacze; }
+private:
+	std::string pseudonim;
+	unsigned sluchacze;
+};
