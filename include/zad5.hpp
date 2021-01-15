63312ad6fed1377e68ddd5159bc57deb4a403649
SajMich <michal.sajewicz.stud@pw.edu.pl>
2021-01-15 08:27:06 +0000

    v8

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -5,3 +5,10 @@
 #include <vector>
 
 // tutaj algorytm liczPopularnych(T begin, T end)
+template <typename T>
+int liczPopularnych(T t1, T t2)
+{
+	int n;
+	n = std::count_if(t1, t2, [](Artysta a) {return a.getSluchacze() > 1000; });
+	return n;
+}
